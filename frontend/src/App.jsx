dbb61dcb98b9258218d25d62a4218db5f12c5733
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-02 23:04:38 +0530

    set up and basic routing complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import FaceExpression from './features/expression/components/FaceExpression'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/AppRouter'
+import { Children } from 'react'
 
 function App() {
   return (
-    <div>
-      <FaceExpression/>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Registration from '../features/auth/pages/Registration'
+import Login from '../features/auth/pages/Login'
+
+export const router = createBrowserRouter([
+    {
+        path:'/registration',
+        element:<Registration/>
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    }
+])
