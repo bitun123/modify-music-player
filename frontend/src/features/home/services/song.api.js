3b62b73af1d5048d5b7fd7efc46dfc696c2e2061
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-05 16:02:56 +0530

    fix: correct createSong function implementation and ensure proper data handling

--- frontend/src/features/home/services/song.api.js
+++ frontend/src/features/home/services/song.api.js
@@ -1,12 +1,30 @@
 import axios from "axios";
 
-const api  = axios.create({
-    baseURL:import.meta.env.VITE_BASE_URL,
-    withCredentials:true
-})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+});
 
+export const createSong = async ({ song, mood }) => {
+  const fromData = new fromData();
+  fromData.append("song", song);
+  fromData.append("mood", mood);
 
+  const response = await api.post("/api/songs/", fromData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
-export const createSong = async ()=>{
-    const 
-}
+  return response.data;
+};
+
+export const savedSong = async (SongId) => {
+  const response = await api.post("/api/savedSongs/" + SongId);
+  return response.savedSong;
+};
+
+export const getAllData = async ({ mood }) => {
+  const response = await api.get("/api/songs?mood=" + mood);
+  return response.song;
+};
