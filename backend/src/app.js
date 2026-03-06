f8b784e6f3c24445e0050dc27740db283349771a
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-06 22:31:33 +0530

    build disk file for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookie = require("cookie-parser");
 const cors = require("cors");
+
 /**
  * @importing Routes
  */
@@ -18,7 +19,7 @@ app.use(
     credentials: true,
   }),
 );
-
+app.use(express.static("./public"))
 
 /**
  * use routes
